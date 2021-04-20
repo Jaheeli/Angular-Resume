644b9b449baea32113f47b7d9fd87f79d4a94924
Jaheeli <brandonbaker16@live.com>
2021-04-20 16:03:06 -0500

    Major update

--- src/app/work-experience/work-experience.service.ts
+++ src/app/work-experience/work-experience.service.ts
@@ -11,31 +11,31 @@ export class WorkExperienceService {
   workExperience: WorkExperience[] = [
     {
       id: 0,
-      title: "Crew Trainer",
-      name: "McDonalds",
-      date: "June 2020 to Present",
-      jobServices: "Train new crew, keep the store running smooth by keeping production up to the golden standard, direct crew in place of management if they are not present, keeping the area of work clean, staying professional at all times",
+      title: "Warehouse Worker",
+      name: "Select Brands",
+      date: "January 2021 to Present",
+      jobServices: "Assist Lead with various tasks, train new warehouse workers, clean warehouse, assist Production team as needed, assist Forklift team as needed.",
     },
     {
       id: 1,
-      title: "Personal Care Aide",
-      name: "LHC Group Inc",
-      date: "January 2020 to July 2020",
-      jobServices: "Train new employees as directed, care for clients, provide clients with services they cannot easily do themselves",
+      title: "Freelance Web Developer and Game Developer",
+      name: "None",
+      date: "January 2020 to Present",
+      jobServices: "Develop various web apps for clients, as well as working with a studio team in making games and 3D art",
     },
     {
       id: 2,
-      title: "Dietary Aide",
-      name: "James River Nursing and Rehab",
-      date: "October 2019 to December 2019",
+      title: "Production Worker",
+      name: "Red Monkey",
+      date: "November 2020 to December 2020",
       jobServices: "Train new employees as directed, care for elderly clients, make food and drinks for the elderly clients",
     },
     {
       id: 3,
-      title: "Assistant Manager",
-      name: "Sonic Drive-In",
-      date: "April 2019 to October 2019",
-      jobServices: "Train new employees, assist employees, count down carhops, count safe, count deposits, close store, manage kitchen, manage front of house, manage shift"
+      title: "Crew Trainer",
+      name: "McDonalds",
+      date: "March 2020 to November 2020",
+      jobServices: "Train new crew, keep the store running smooth by keeping production up to the golden standard, direct crew in place of management if they are not present, keeping the area of work clean, staying professional at all times"
     }
   ];
 
